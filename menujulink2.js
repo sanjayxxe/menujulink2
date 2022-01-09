e9b180a0053a93814d948c390c7ca01a1c24ea3c
sanjayxxe <96386386+sanjayxxe@users.noreply.github.com>
2022-01-09 15:50:12 +0700

    Update menujulink2.js

--- menujulink2.js
+++ menujulink2.js
@@ -42,7 +42,7 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="safelink.html"}
- a_to_ve[i].href="https://linku-kordramalover.blogspot.com/2022/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"n";}} var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
+if(x=="1"){xxx="generate.html"}
+ a_to_ve[i].href="https://linku-kordramalover.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"n";}} var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;} if(a_to_vk) {a_to_vk.innerHTML+=a_to_va;}}
 function a_to_fa() {var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
